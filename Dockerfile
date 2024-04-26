1851ab1bc23875d8fc6851ef47b6c239dd2dfeb6
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2024-04-26 18:19:13 +0530

    Deleted all files

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3
-
-COPY . .
-
-CMD ["python3" , " start.py"]
--- start.py
+++
@@ -1,4 +0,0 @@
-print("This is a end to end CI/CD project")
-print("Created by Hrithik Chauhan")
-print("This is used to tese and build CI pipeline")
-print("And learn how to create jenkins pipelines for CI/CD")
