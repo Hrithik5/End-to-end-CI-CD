f11159df0868ebe0067027adc4cc40478bc2e970
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2024-05-07 00:20:12 +0530

    Added Desc in app.py file

--- aws-cicd/app.py
+++ aws-cicd/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from AWS CICD Pipelinee'
+    return 'AWS CICD Pipline for a python based Flask app'
 
 'if __name__ == "__main__":
     app.run()
